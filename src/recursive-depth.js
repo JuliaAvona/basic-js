02ba21bd2e468eb2124c2977ef987cf79c884bfa
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-03-08 11:38:28 -0600

    init: sort-by-height

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,24 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+// class DepthCalculator {
+//   calculateDepth(arr) {
+//     let count = 0;
+//     let first = arr.flat(Infinity);
+//     let second = arr.flat(count);
+
+//     while (JSON.stringify(first) !== JSON.stringify(second)) {
+//       count++;
+//       second = arr.flat(count);
+//     }
+//     return count + 1;
+//   }
+// }
+
 class DepthCalculator {
   calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 }
 
 module.exports = {
-  DepthCalculator
+  DepthCalculator,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let newArr = [];
+  let clone = arr.slice(0);
+  let arrSort = clone.sort((a , b) => a - b).filter(item => item !== -1);
+  let itemSortArr = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+        newArr.push(-1);  
+    } else {
+        newArr.push(arrSort[itemSortArr]);
+        itemSortArr++;
+    }
+  }
+  return newArr;
 }
 
 module.exports = {
